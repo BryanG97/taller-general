833e325f92a51eb565a54cb43a4ac5e761e3cad4
Bryan Guevara <bguevara@ec.krugercorp.com>
2025-01-25 13:01:22 -0500

    Cambio

--- first-api-rest/src/test/java/com/first/api/ProductoControllerTest.java
+++ first-api-rest/src/test/java/com/first/api/ProductoControllerTest.java
@@ -91,6 +91,7 @@ public class ProductoControllerTest {
         String resultado = productoController.eliminarProducto(1);
 
         assertNotNull(resultado);
+        
         assertEquals("Producto eliminado !!1", resultado);
         verify(productoService, times(1)).deleteProducto(1);
     }
